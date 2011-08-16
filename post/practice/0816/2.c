65ff3c5006be51d06c352e0a5a26904016267a6c
ymqqqqdx <ymqqqqdx@gmail.com>
2011-08-16 15:36:58 +0800

    a

--- post/practice/0816/2.c
+++ post/practice/0816/2.c
@@ -4,9 +4,9 @@ int main(int argc, const char *argv[])
 {
     int i, j, k;
     int count = 0;
-    for (i = 0; i <= 4; i++) 
-        for (j = 0; j <= 4; j++) 
-            for (k = 0; k <= 4; k++) 
+    for (i = 1; i <= 4; i++) 
+        for (j = 1; j <= 4; j++) 
+            for (k = 1; k <= 4; k++) 
                 if (i != j && i != k && j != k) 
                 {
                     count++;
